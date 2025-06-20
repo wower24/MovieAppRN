4ef06f4dfafcab37607f47301b9dc27befc4ee71
Marta Wower <marta.wower24@gmail.com>
2025-06-19 22:25:54 -0600

    Styling Setup
    Setup Nativewind and explored how it works

--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Welcome!</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-5xl text-dark-200 font-bold">Welcome!</Text>
     </View>
   );
 }
