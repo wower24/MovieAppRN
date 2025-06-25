2475e3bf851d8266bba985a8f88edf965c7112ce
Marta Wower <marta.wower24@gmail.com>
2025-06-25 09:57:34 -0600

    MovieCard customization
    Customized movie card by adding a rating, release date and original language under the movie poster.

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,16 +1,18 @@
+import { icons } from '@/constants/icons'
 import { Link } from 'expo-router'
 import React from 'react'
-import { Image, Text, TouchableOpacity } from 'react-native'
+import { Image, Text, TouchableOpacity, View } from 'react-native'
 
 const MovieCard = ({
     id, 
     poster_path, 
     title,
     vote_average,
-    release_date
+    release_date,
+    original_language
 }: Movie) => {
   return (
-    <Link href={`./movie/${id}`} asChild>
+    <Link href={`/movies/${id}`} asChild>
         <TouchableOpacity className="w-[30%]">
             <Image 
                 source={{
@@ -22,7 +24,33 @@ const MovieCard = ({
                 resizeMode = "cover"
             />
 
-            <Text className="text-sm text-white font-bold mt-2">{title}</Text>  
+            <Text 
+                className="text-sm text-white font-bold mt-2"
+                numberOfLines={1}    
+            >
+                {title}
+            </Text>
+
+            <View className="flex-row items-center justify-start gap-x-1">
+                <Image 
+                    source={icons.star}
+                    className="size-4"
+                />
+
+                <Text className="text-white text-xs font-bold uppercase">
+                    {Math.round(vote_average / 2)}
+                </Text>
+            </View>  
+
+            <View className="flex-row items-center justify-between">
+                <Text className="text-xs text-light-300 font-medium mt-1">
+                    {release_date?.split('-')[0]}
+                </Text>
+
+                <Text className="text-xs text-light-300 font-medium mt-1 me-1">
+                    {original_language.toUpperCase()}
+                </Text>
+            </View>
         </TouchableOpacity>
     </Link>
   )
