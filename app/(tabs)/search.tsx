681747c1723b0d5dad57b52faa66daeff9ea63a5
Marta Wower <marta.wower24@gmail.com>
2025-06-27 22:55:52 -0600

    Appwrite set up
    Set up Appwrite client and database.
    Created a function to count how many times a term was searched and fetched its first movie.

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -3,6 +3,7 @@ import SearchBar from '@/components/SearchBar';
 import { icons } from '@/constants/icons';
 import { images } from '@/constants/images';
 import { fetchMovies } from '@/services/api';
+import { updateSearchCount } from '@/services/appwrite';
 import useFetch from '@/services/useFetch';
 import React, { useEffect, useState } from 'react';
 import { ActivityIndicator, FlatList, Image, Text, View } from 'react-native';
@@ -24,9 +25,14 @@ const Search = () => {
   );
 
   useEffect(() => {
+
     const timeoutId = setTimeout( async () => {
       if (searchQuery.trim()) {
         await loadMovies();
+
+        if(movies?.length > 0 && movies?.[0]) {
+          await updateSearchCount(searchQuery, movies[0]);
+        }
       } else {
         reset();
       }
---
+++ services/appwrite.ts
@@ -0,0 +1,58 @@
+import { Client, Databases, ID, Query } from 'react-native-appwrite';
+
+const DATABASE_ID = process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!;
+const COLLECTION_ID = process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID!;
+
+const client = new Client()
+    .setEndpoint(process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!)
+    .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!);
+
+const database = new Databases(client);
+
+//track searches made by the user
+export const updateSearchCount = async (query: string, movie: Movie) => {
+
+    try {
+        const result = await database.listDocuments(
+            DATABASE_ID,
+            COLLECTION_ID,
+            [
+                Query.equal('searchTerm', query)
+            ]
+        )
+
+        console.log(result);
+        // check if a record of that search has already been stored
+        if (result.documents.length > 0) {
+            const existingMovie = result.documents[0];
+
+            await database.updateDocument(
+                DATABASE_ID,
+                COLLECTION_ID,
+                existingMovie.$id,
+                {
+                    // if yes, increment the count
+                    count: existingMovie.count + 1
+                }
+            )
+        } else {
+            await database.createDocument(
+                DATABASE_ID,
+                COLLECTION_ID,
+                ID.unique(),
+                {
+                    searchTerm: query,
+                    movie_id: movie.id,
+                    count: 1,
+                    title:  movie.title,
+                    poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+                }
+            )
+        }
+        // if not, create a new record with count 1
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+
+} 
