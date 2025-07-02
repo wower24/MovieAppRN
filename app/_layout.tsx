82088f26cc4993e56d5fc33b9c9280914ef901d5
Marta Wower <marta.wower24@gmail.com>
2025-07-01 22:44:01 -0600

    Hide StatusBar
    Modified _layout.tsx to hide the time and battery (StatusBar)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,20 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "react-native";
 import './globals.css';
 
 export default function RootLayout() {
-  return <Stack>
-	<Stack.Screen
-		name = "(tabs)"
-		options={{headerShown: false}}
-	/>
-	<Stack.Screen
-		name = "movie/[id]"
-		options={{headerShown: false}}
-	/>
-</Stack>
+	return <>
+		<StatusBar hidden={true} />
+		<Stack>
+			<Stack.Screen
+				name="(tabs)"
+				options={{ headerShown: false }}
+			/>
+			<Stack.Screen
+				name="movie/[id]"
+				options={{ headerShown: false }}
+			/>
+		</Stack>
+	</>
 
 }
