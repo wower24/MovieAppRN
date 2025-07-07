0511d92451252b23f33489e6a90c77587989d6ad
Marta Wower <marta.wower24@gmail.com>
2025-07-07 12:25:43 -0600

    Added TODOs
    Modified pages that are not done yet so they look nicer an indicate that this is something I have to do later.

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,21 @@
+import { icons } from '@/constants/icons'
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Image, Text, View } from 'react-native'
 
 const profile = () => {
   return (
-    <View>
-      <Text>profile</Text>
+    <View className='bg-primary flex-1 px-10'>
+      <View className='flex justify-center items-center flex-1 flex-col gap-5'>
+        <Image 
+        source={icons.person} 
+        className="size-10"
+        tintColor="#FFF" 
+        />
+        <Text className="text-white font-normal text-xl">
+          TODO: Profile Page
+        </Text>
+      </View>
+      
     </View>
   )
 }
--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,11 +1,22 @@
-import { View, Text } from 'react-native'
+import { icons } from '@/constants/icons'
 import React from 'react'
+import { Image, Text, View } from 'react-native'
 
 const saved = () => {
   return (
-    <View>
-      <Text>saved</Text>
-    </View>
+    <View className='bg-primary flex-1 px-10'>
+          <View className='flex justify-center items-center flex-1 flex-col gap-5'>
+            <Image 
+            source={icons.save} 
+            className="size-10"
+            tintColor="#FFF" 
+            />
+            <Text className="text-white font-normal text-xl">
+              TODO: Saved Page
+            </Text>
+          </View>
+          
+        </View>
   )
 }
 
