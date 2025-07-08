8bda6e7a1c138e4bb87fa7aeb99fd900d394dffd
Marta Wower <marta.wower24@gmail.com>
2025-07-08 15:24:22 -0600

    Profile Page
    Changed background, added logo and moved icon and text up

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,21 +1,27 @@
 import { icons } from '@/constants/icons'
+import { images } from '@/constants/images'
 import React from 'react'
 import { Image, Text, View } from 'react-native'
 
 const profile = () => {
   return (
-    <View className='bg-primary flex-1 px-10'>
-      <View className='flex justify-center items-center flex-1 flex-col gap-5'>
-        <Image 
-        source={icons.person} 
-        className="size-10"
-        tintColor="#FFF" 
+    <View className='bg-primary flex-1'>
+      <Image source={images.bg} className="absolute w-full z-0" />
+      <Image
+        source={icons.logo}
+        className="w-12 h-10 mt-20 mx-auto"
+      />
+      <View className='flex items-center flex-1 flex-col gap-5 mt-20'>
+        <Image
+          source={icons.person}
+          className="size-20"
+          tintColor="#FFF"
         />
-        <Text className="text-white font-normal text-xl">
-          TODO: Profile Page
+        <Text className="text-light-200 font-normal text-xl">
+          Username
         </Text>
       </View>
-      
+
     </View>
   )
 }
